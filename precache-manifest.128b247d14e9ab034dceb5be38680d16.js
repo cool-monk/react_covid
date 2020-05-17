9e8a49d09389bae1c766611372bda3db356ef85b
cool-monk <raj123.sah@gmail.com>
2020-05-17 09:11:19 +0530

    Updates

---
+++ precache-manifest.128b247d14e9ab034dceb5be38680d16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c79a6d65f3c7703834d9b416a5c9a05",
+    "url": "/react_covid/index.html"
+  },
+  {
+    "revision": "271eb64877374c446ad5",
+    "url": "/react_covid/static/css/main.a418ab99.chunk.css"
+  },
+  {
+    "revision": "0a3df374ea13fa27021f",
+    "url": "/react_covid/static/js/2.f3ddd698.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react_covid/static/js/2.f3ddd698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "271eb64877374c446ad5",
+    "url": "/react_covid/static/js/main.64da1c6d.chunk.js"
+  },
+  {
+    "revision": "e45d7ac6137fc60b82ff",
+    "url": "/react_covid/static/js/runtime-main.e7ed4363.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react_covid/static/media/image.d7265326.png"
+  }
+]);
